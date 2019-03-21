f14ae961bda405dbe31988dd2756cf8717f3a8fb
Lauren Boutros <lauren>
2019-03-20 20:45:27 -0400

    all but font and inputfield

---
+++ project2/js/popup.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('whatever the id is').click(function()){
+		$("#whatever the id is").toggleClass("visible");
+		$(".modal").toggleClass("visible");
+	});
+	)};
+}
+
+$(document).ready(function(){
+	$("#whatever the id is").click(function()){
+		$("#whatever the id is").toggleClass("visible)");
+		$(".modal").toggleClass("visible");
+		
+	}
+}
