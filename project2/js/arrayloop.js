2a693807e182d4f82316db80c16514f8f253cd79
Lauren Boutros <lauren>
2019-03-21 11:23:28 -0400

    still remaining: spell input field, popup modals, game feedack, Dolch array loop

---
+++ project2/js/arrayloop.js
@@ -0,0 +1,6 @@
+var myStringArray = ["Hello","World"];
+    var arrayLength = myStringArray.length;
+    for (var i = 0; i < arrayLength; i++) {
+        console.log(myStringArray[i]);
+        //Do something
+    }
