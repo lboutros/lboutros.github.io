d87b507ddc0dda53580154d4e6f80dfee9f6b663
Lauren Boutros <lauren>
2019-03-24 19:48:54 -0400

    fixed about popup placement

--- project2/js/arrayloop.js
+++ project2/js/arrayloop.js
@@ -7,3 +7,16 @@ for(i = 0; i < dolch.length; i++){
 }
 
 // on click, go to next item in array
+
+////////////////////////////////////////// ARRAY LOOP /////////////////////////////////////
+
+
+var dolch = ["brown", "carry", "where", "three", "funny"]
+
+$("#tap-right").click(
+		function(){
+			for(i = 0; i < dolch.length; i++){
+				if(dolch[i] = "brown"){
+				$(".word-container").text(dolch[i]);
+	});
+});
